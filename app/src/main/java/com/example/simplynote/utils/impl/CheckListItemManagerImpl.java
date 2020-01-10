a72ae6c867f799b35f0daf6d0045541e86c18d2c
BartekK <bartek.kesicki25@gmail.com>
2020-01-10 19:03:32 +0100

    fixed adding rows to create new checklist

--- app/src/main/java/com/example/simplynote/utils/impl/CheckListItemManagerImpl.java
+++ app/src/main/java/com/example/simplynote/utils/impl/CheckListItemManagerImpl.java
@@ -11,6 +11,7 @@ import com.example.simplynote.R;
 import com.example.simplynote.new_checklist.OnChecklistItemClickListener;
 import com.example.simplynote.utils.ChecklistItemManager;
 import com.example.simplynote.utils.StringProvider;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -18,6 +19,7 @@ import javax.inject.Inject;
 
 public class CheckListItemManagerImpl implements ChecklistItemManager {
 
+    private static final int DEFAULT_CHECKLIST_ITEM_WIDTH = 100;
     private Context mContext;
     private StringProvider stringProvider;
 
@@ -30,10 +32,9 @@ public class CheckListItemManagerImpl implements ChecklistItemManager {
     public LinearLayout createNewRow(OnChecklistItemClickListener onChecklistItemClickListener, int id) {
         LinearLayout linearLayout = new LinearLayout(mContext);
         linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-        //todo change Linear to Constraint Layout
         TextInputLayout textInputLayout = createTextInputLayout();
         linearLayout.addView(textInputLayout);
-        Button removeItemButton = createRemoveItemButton(onChecklistItemClickListener, id);
+        FloatingActionButton removeItemButton = createRemoveItemButton(onChecklistItemClickListener, id);
         linearLayout.addView(removeItemButton);
         return linearLayout;
     }
@@ -43,11 +44,11 @@ public class CheckListItemManagerImpl implements ChecklistItemManager {
         this.mContext = context;
     }
 
-    private Button createRemoveItemButton(final OnChecklistItemClickListener onChecklistItemClickListener, final int id) {
-        Button button = new Button(mContext);
+    private FloatingActionButton createRemoveItemButton(final OnChecklistItemClickListener onChecklistItemClickListener, final int id) {
+        FloatingActionButton button = new FloatingActionButton(mContext);
         button.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        //todo replace text on button with icon
-        button.setText(stringProvider.getString(R.string.list_item_remove_button_text));
+        button.setBackgroundColor(mContext.getColor(R.color.colorBlack));
+        button.setImageResource(android.R.drawable.ic_delete);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,9 +64,13 @@ public class CheckListItemManagerImpl implements ChecklistItemManager {
         textInputLayout.setBoxBackgroundMode(TextInputLayout.BOX_BACKGROUND_OUTLINE);
         textInputLayout.setBoxCornerRadii(5, 5, 5, 5);
         TextInputEditText editext = new TextInputEditText(textInputLayout.getContext());
-        textInputLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        textInputLayout.setLayoutParams(new LinearLayout.LayoutParams(calculateDP(300), LinearLayout.LayoutParams.WRAP_CONTENT));
         editext.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
         textInputLayout.addView(editext);
         return textInputLayout;
+    }
+
+    private int calculateDP(int px) {
+        return (int)(px * mContext.getResources().getDisplayMetrics().density);
     }
 }
